4e7dd66b19f891a98195a716762555be7ae86958
matevarju89 <matevarju89@gmail.com>
2020-04-24 16:47:21 +0200

    added about.js

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,11 @@
-const index = () => <h1 className='hello'>Hello everybody from next.js</h1>;
+import Link from 'next/link';
+
+const index = () => (
+  <section>
+    <h1 className='hello'>Hello everybody from next.js</h1>
+    <Link href='/about'>
+      <a title='About Next'>About this project</a>
+    </Link>
+  </section>
+);
 export default index;
